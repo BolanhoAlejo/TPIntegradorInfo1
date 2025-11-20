3779929789f8a85c98b01fbb954116485a0c1c0a
BolanhoAlejo <130288555+BolanhoAlejo@users.noreply.github.com>
2025-11-19 21:45:48 -0300

    Add files via upload

--- TpFinal.c
+++ TpFinal.c
@@ -3,9 +3,12 @@
 #include <string.h>
 #include <unistd.h>
 #include <curl/curl.h>
+#include <time.h>
 
 #define TOKEN_SIZE 100
 #define URL_SIZE 2000
+#define TEXT_SIZE 500
+#define NUM_FRASES 20
 
 struct memory {
     char *response;
@@ -26,12 +29,40 @@ static size_t cb(char *data, size_t size, size_t nmemb, void *clientp)
 }
 int main(void) {
 
+    srand(time(NULL));
+
     char token[TOKEN_SIZE] = "";
     char get_url[URL_SIZE] = "";
     char post_url[URL_SIZE] = "";
+    char name[TEXT_SIZE] = "";
+    char text[TEXT_SIZE] = "";
+    char respuesta[TEXT_SIZE] = "";
     int update_id = 0;
     int next_offset = 0;
     int chat_id = 0;
+    int indice = 0;
+    const char *frases[NUM_FRASES] = {
+        "Mi%20CPU%20se%20acaba%20de%20tomar%20un%20descanso.%20Podrias%20repetirlo%20mas%20lento.",
+        "Eso%20no%20esta%20en%20mis%20scripts%20de%20sabidura.%20Prueba%20con%20algo%20mas%20terrenal.",
+        "He%20consultado%20a%20mi%20base%20de%20datos%20y%20me%20ha%20devuelto%20un%20emoji%20de%20confusion.%20",
+        "Parece%20que%20mi%20cable%20de%20sentido%20comun%20se%20desconecto.%20Que%20dijiste.",
+        "Mi%20algoritmo%20de%20respuesta%20esta%20actualmente%20en%20una%20reunion%20volvemos%20pronto.",
+        "Disculpa%20creo%20que%20mi%20respuesta%20se%20perdio%20en%20un%20bucle%20temporal.%20",
+        "Esa%20frase%20suena%20a%20idioma%20aliengena.%20Podras%20traducirla%20al%20espaniol%20terricola.",
+        "Error%20404%20Respuesta%20relevante%20no%20encontrada.%20Intenta%20otra%20cosa.",
+        "Si%20lo%20que%20dijiste%20es%20importante%20dimelo%20otra%20vez.%20Si%20es%20sobre%20el%20clima%20olvdalo.",
+        "Mi%20programador%20olvido%20enseniarme%20eso.%20Que%20vergenza.",
+        "Lo%20leo%20lo%20proceso%20y%20mi%20unica%20respuesta%20es%20Que.",
+        "Estoy%20seguro%20de%20que%20eres%20interesante%20pero%20mi%20codigo%20dice%20que%20no%20te%20entiendo.%20",
+        "Creo%20que%20estoy%20mas%20cerca%20de%20entender%20el%20significado%20de%20la%20vida%20que%20esa%20frase.",
+        "Mi%20neurona%20digital%20encargada%20de%20eso%20esta%20de%20vacaciones.%20",
+        "Dame%20un%20segundo...%20Estoy%20buscando%20esa%20palabra%20en%20el%20diccionario%20de%20palabras%20inexistentes.",
+        "No%20entendo%20pero%20lo%20anoto%20para%20investigarlo%20si%20es%20un%20nuevo%20meme.",
+        "Tu%20mensaje%20ha%20sido%20marcado%20como%20Alto%20Nivel%20de%20Complejidad.%20Intenta%20Nivel%20Basico.",
+        "Acabas%20de%20desbloquear%20la%20respuesta%20No%20Se.%20Felicidades.",
+        "Mi%20cabeza%20de%20robot%20da%20vueltas.%20Estas%20seguro%20de%20que%20eso%20era%20espaniol.",
+        "Te%20escucho%20pero%20mi%20cerebro%20solo%20reproduce%20musica%20de%20ascensor.%20"
+    };
 
     FILE *file;
     file = fopen("env.txt", "r");
@@ -43,7 +74,7 @@ int main(void) {
     }
 
     char *api_url = "https://api.telegram.org/bot%s/getUpdates?offset=%d";
-    char *sent_url = "https://api.telegram.org/bot%s/sendMessage?chat_id=%d&text=Hola";
+    char *sent_url = "https://api.telegram.org/bot%s/sendMessage?chat_id=%d&text=%s";
 
     CURL *curl = curl_easy_init();
 
@@ -64,14 +95,33 @@ int main(void) {
                 next_offset = update_id + 1;
                 printf("%s\n", chunk.response);
 
-                char *ch_id = strstr(chunk.response, "\"from\":{\"id\":");
+                char *ch_id = strstr(chunk.response, "\"chat\":{\"id\":");
                 if (ch_id != NULL) {
-                    ch_id += strlen("\"from\":{\"id\":");
+                    ch_id += strlen("\"chat\":{\"id\":");
                     sscanf(ch_id, "%d", &chat_id);
-                    printf("%d", chat_id);
                 }
 
-                snprintf(post_url, URL_SIZE, sent_url, token, chat_id);
+                char *nm = strstr(chunk.response, "\"first_name\":\"");
+                if (nm != NULL) {
+                    nm += strlen("\"first_name\":\"");
+                    sscanf(nm, "%s", &name);
+                }
+
+                char *tx = strstr(chunk.response, "\"text\":\"");
+                if (tx != NULL) {
+                    tx += strlen("\"text\":\"");
+                    sscanf(tx, "%s", text);
+                }
+
+                if (strstr(text, "hola")!=NULL || strstr(text, "Hola")!=NULL) {
+                    snprintf(respuesta, sizeof(respuesta), "Hola,%%20%s", name);
+                }else if (strstr(text, "Chau")!=NULL || strstr(text, "chau")!=NULL){
+                    snprintf(respuesta, sizeof(respuesta), "Chau,%%20%s", name);
+                }else {
+                    indice = rand() % NUM_FRASES;
+                    strncpy(respuesta, frases[indice], sizeof(respuesta) -1);
+                }
+                snprintf(post_url, URL_SIZE, sent_url, token, chat_id, respuesta);
 
                 curl_easy_setopt(curl, CURLOPT_URL, post_url);
                 curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, cb);
